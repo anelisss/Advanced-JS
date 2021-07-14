a162df37723367939cc0dcde80a4dec475286dc6
AnelisSuliman <ane.lis07@yahoo.com>
2021-07-14 17:13:31 +0300

    calculates hours with vuejs

--- vuejs/hello.js
+++ vuejs/hello.js
@@ -1,7 +1,52 @@
+/* vueJs: */
+
 let app = new Vue({
     el: '#app',
     data: {
-        message: "Test",
-        time: new Date().toTimeString()
+        // message: "Test",
+        // time: new Date().toTimeString()
+        counter: 0
+    },
+    methods: {
+        increment() {
+            this.counter++
+        }
+    },
+    computed: {
+        time() {
+            let time = new Date()
+            return time.toTimeString()
+        },
+        message() {
+            let hour = new Date().getHours()
+            if (hour < 12 && hour > 1) {
+                return ("Buna dimineata!")
+            } else if (hour >= 12 && hour < 18) {
+                return ("Buna ziua!")
+            } else if (hour >= 18 && hour <= 23) {
+                return ("Buna seara!")
+            } else {
+                return ("Somn Usor!")
+            }
+        }
     }
 })
+
+/*
+    Vanilla JS:
+
+        let time = new Date().toTimeString();
+        document.querySelector('#now').innerText = time;
+
+        let message = "Test";
+        document.querySelector("p:firstChild()").innerText = message;
+
+
+    ~calcul nr de clickuri pe input btn:
+
+        let counter = 0;
+        document.querySelector('.clicks').addEventListener('click', function() {
+            counter++;
+            document.querySelector('.clicks').value = counter;
+        })
+*/
